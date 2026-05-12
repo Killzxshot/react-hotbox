041d12adbdaf3070bacb979cd5c0f25c9c130148
lane <Killzxshot@users.noreply.github.com>
2026-05-12 07:00:39 +0200

    fix: Fix build configuration

--- src/fix_build_configuration_0.ts
+++ src/fix_build_configuration_0.ts
@@ -2,14 +2,13 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  outDir: 'dist',
   format: ['esm', 'cjs'],
   splitting: false,
   sourcemap: true,
   clean: true,
+  minify: true,
   dts: true,
-  minify: false,
-  target: 'es2020',
   external: ['react'],
   bundle: true,
+  target: 'es2020',
 });
